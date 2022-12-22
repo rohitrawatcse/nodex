06de370d925d302d328957ee6b0d4bacfa7f643b
Rohit Rawat <rohitrawatcse@gmail.com>
2022-12-22 11:01:49 +0530

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+const express =  require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+    res.send("Hi, I am live ");
+});
+
+const start = async ()=>{
+    try{
+        app.listen(PORT,()=>{
+         console.log(`${PORT} Yes I am Connected`)
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+
+
+start();
